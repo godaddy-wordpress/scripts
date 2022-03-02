0897072503328f5bebd5aca2411960d4daf8b97c
AnthonyLedesma <anthonymledesma@gmail.com>
2022-03-02 15:08:52 -0700

    apply linting

--- utils/index.js
+++ utils/index.js
@@ -1,25 +1,27 @@
-const { createWriteStream } = require('fs');
-const { pipeline } = require('stream');
-const { promisify } = require('util');
-const AdmZip = require('adm-zip');
+const { createWriteStream } = require( 'fs' );
+const { pipeline } = require( 'stream' );
+const { promisify } = require( 'util' );
+const AdmZip = require( 'adm-zip' );
 
-const fetch = (...args) => import('node-fetch').then(({ default: asyncFetch }) => asyncFetch(...args));
+// Disable reason: Not sure why this is unsupported because Node 17 should support es.
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+const fetch = ( ...args ) => import( 'node-fetch' ).then( ( { default: asyncFetch } ) => asyncFetch( ...args ) );
 
-const download = async (url, path) => {
-  const streamPipeline = promisify(pipeline);
+const download = async ( url, path ) => {
+	const streamPipeline = promisify( pipeline );
 
-  const response = await fetch(url);
+	const response = await fetch( url );
 
-  if (!response.ok) {
-    throw new Error(`unexpected response ${response.statusText}`);
-  }
+	if ( ! response.ok ) {
+		throw new Error( `unexpected response ${ response.statusText }` );
+	}
 
-  await streamPipeline(response.body, createWriteStream(path));
+	await streamPipeline( response.body, createWriteStream( path ) );
 };
 
-const unzip = async (file, path) => {
-  const zip = new AdmZip(file);
-  zip.extractAllTo(path, true);
+const unzip = async ( file, path ) => {
+	const zip = new AdmZip( file );
+	zip.extractAllTo( path, true );
 };
 
 exports.download = download;
